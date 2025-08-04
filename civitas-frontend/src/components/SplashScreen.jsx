c42c3baecacc96688bb3f28db884f43fda675f8e
Bhuvan M S <bhuvanms97@gmail.com>
2025-08-04 12:46:06 +0530

    required changes are done

--- civitas-frontend/src/components/SplashScreen.jsx
+++ civitas-frontend/src/components/SplashScreen.jsx
@@ -16,7 +16,7 @@ function SplashScreen() {
   return (
     <div className={styles.splashWrapper}>
       <div className={styles.splashContainer}>
-        <img src={`${process.env.PUBLIC_URL}/images/logo2.png`} alt="CIVITAS Logo" className={styles.logo} />
+        <img src={`${process.env.PUBLIC_URL}/images/logo2.PNG`} alt="CIVITAS Logo" className={styles.logo} />
 
         <h1 className={styles.title}>CIVITAS</h1>
         <p className={styles.tagline}>Empowering Citizens, Building Communities</p>
